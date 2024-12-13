bc13bfd1edc47c56fa15dd8aea695645e095bad6
Rahil Raipuria <rahil.raipuria2003@gmail.com>
2024-12-13 21:43:09 +0530

    browseer-ext modified

--- server/src/index.js
+++ server/src/index.js
@@ -1,17 +1,72 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import {app} from "./app.js";
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import cors from "cors";
+
+
+const app = express();
+
 
 dotenv.config({
-    path:'./.env'
+  path: './.env' 
 });
 
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*", 
+    methods: ["GET", "POST"]
+  }
+});
+
+
+app.use(cors());
+
+
 connectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 8000,()=>{
-        console.log(`Server running on ${process.env.PORT}`);
-    });   
-})
-.catch((err)=>{
-    console.log("Mongo DB connnection failed!!!",err)
-})
+  .then(() => {
+    const port = process.env.PORT || 8000;
+    server.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection failed!!!", err);
+  });
+
+
+app.use(express.static('public'));
+
+
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+
+  
+  socket.on('tabVisibilityChanged', (data) => {
+    console.log('Tab visibility changed:', data);
+    
+  });
+
+  
+  socket.on('windowFocusChanged', (data) => {
+    console.log('Window focus state changed:', data);
+  });
+
+  
+  socket.on('clipboardEvent', (data) => {
+    console.log(`${data.action} event received. Content:`, data.content);
+  });
+
+  
+  socket.on('fullscreenChanged', (data) => {
+    console.log('Fullscreen state changed:', data);
+  });
+
+  
+  socket.on('disconnect', () => {
+    console.log('A user disconnected:', socket.id);
+  });
+});
