05947784a54738c0c32638f4765d9ead666caf58
Pratyush Kumar <pratyushk512@gmail.com>
2024-12-13 11:39:34 +0530

    client side fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,6 @@
 import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Consumer from './pages/consumer.jsx';
-import Role from './pages/rewards-role.jsx';
-import Landing from './pages/Landing';
-import Qr from './pages/qr.jsx';
-import Previousreports from './pages/Previousreports.jsx';
-import Login from './pages/login';
-import SignUp from './pages/signup';
-import ScanProduct from './pages/scan-products';
-import Report from './pages/report.jsx';
-import Home from './pages/home.jsx';
-import Market from './pages/market.jsx';
+
 
 function App() {
   const router = createBrowserRouter([
@@ -18,46 +8,6 @@ function App() {
       path: "/",
       element: <Home />,
     },
-    {
-      path: "/login",
-      element: <Login />,
-    },
-    {
-      path: "/signup",
-      element: <SignUp />,
-    },
-    {
-      path: "/reports",
-      element: <Report />
-    },
-    {
-      path: "/scanProduct",
-      element: <ScanProduct />
-    },
-    {
-      path: "/pastScans",
-      element: <Previousreports />
-    },
-    {
-      path: "/rewards",
-      element: <Role />
-    },
-    {
-      path: "rewards/brands",
-      element: <Qr />
-    },
-    {
-      path: "/rewards/consumer",
-      element: <Consumer />
-    },
-    {
-      path: "/Landing",
-      element: <Landing />
-    },
-    {
-      path: "/market",
-      element: <Market />
-    }
   ]);
 
   return (
