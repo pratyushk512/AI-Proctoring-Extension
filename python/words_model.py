87c62b07c27a3097ae05ac9eb59e2c8f86a90eb9
Rajdeep37 <rajdeep.1si22ad037@gmail.com>
2024-12-14 10:19:48 +0530

    removing xgboost

--- python/words_model.py
+++ python/words_model.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-import xgboost as xgb
+#import xgboost as xgb
 import joblib
 
 data = pd.read_csv('typing_data_high_variance.csv')
@@ -29,10 +29,10 @@ rf_classifier.fit(X_train, y_train)
 
 y_pred_rf = rf_classifier.predict(X_test)
 
-xgb_classifier = xgb.XGBClassifier(use_label_encoder=False, eval_metric='logloss')
-xgb_classifier.fit(X_train, y_train)
+#xgb_classifier = xgb.XGBClassifier(use_label_encoder=False, eval_metric='logloss')
+#xgb_classifier.fit(X_train, y_train)
 
-y_pred_xgb = xgb_classifier.predict(X_test)
+#y_pred_xgb = xgb_classifier.predict(X_test)
 
 def accuracy(y_true, y_pred):
     return np.sum(y_true == y_pred) / len(y_true)
@@ -46,23 +46,23 @@ print("Random Forest Results:")
 print("Accuracy:", accuracy(y_test, y_pred_rf))
 print("Specificity:", specificity(y_test, y_pred_rf))
 
-print("\nXGBoost Results:")
+"""print("\nXGBoost Results:")
 print("Accuracy:", accuracy(y_test, y_pred_xgb))
-print("Specificity:", specificity(y_test, y_pred_xgb))
+print("Specificity:", specificity(y_test, y_pred_xgb))"""
 
 sample_indices = np.random.choice(len(X_train), size=10, replace=False)
 sample_X = X_train[sample_indices]
 sample_y_true = y_train.iloc[sample_indices]
 
 sample_y_pred_rf = rf_classifier.predict(sample_X)
-sample_y_pred_xgb = xgb_classifier.predict(sample_X)
+#sample_y_pred_xgb = xgb_classifier.predict(sample_X)
 
 print("\nSample Predictions vs Actual for Random Forest:")
 for i in range(len(sample_y_true)):
     print(f"Actual: {sample_y_true.iloc[i]}, Predicted (RF): {sample_y_pred_rf[i]}")
 
-print("\nSample Predictions vs Actual for XGBoost:")
+"""print("\nSample Predictions vs Actual for XGBoost:")
 for i in range(len(sample_y_true)):
-    print(f"Actual: {sample_y_true.iloc[i]}, Predicted (XGB): {sample_y_pred_xgb[i]}")
+    print(f"Actual: {sample_y_true.iloc[i]}, Predicted (XGB): {sample_y_pred_xgb[i]}")"""
 
 joblib.dump(rf_classifier, 'random_forest_classifier.pkl')
